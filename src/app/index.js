e815b0cee6c5ca0b1df59eaca87f914f361571e2
ivananutrition <ivana.arizanovic@hotmail.com>
2018-04-15 19:25:41 +0200

    add basic application structure

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+    
+   
+    	render() {
+    		return(
+    			<div>
+    				<h1>ToDoApp</h1>
+    			</div>
+
+    );
+}
+}
+
+export default App;
