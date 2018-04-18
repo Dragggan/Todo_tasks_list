04e4e323178b7ddf3f03d7230820cc569b81689d
Dragggan <dragan1809989@gmail.com>
2018-04-18 17:13:46 +0200

    redux,and router instaled

--- src/app/index.js
+++ src/app/index.js
@@ -1,42 +1,17 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import SignIn from '../containers/signIn/SignIn.js';
-import AddTask from '../containers/addTask'
 
-class App extends Component {
+
+class App extends React.Component {
     
-
-
+   
     	render() {
     		return(
     			<div>
     				<h1>ToDoApp</h1>
-             <div className="test">Testing login
-              <SignIn />
-             </div>
-=======
-import { BrowserRouter as Router } from "react-router-dom";
+    			</div>
 
-import Main from '../components/main'
->>>>>>> 2adacd29e3dc293abace7997dd1c810d83a16b7d
-
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Router>
-        <div>
-            
-            <Main/>
-            
-          </div>
-        </Router>
-      </div>
-      
     );
-  }
+}
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
 import App from "./app";
-import store from './store' //moramo da ubezemo stori koji kupi statate iz reducera
 
-ReactDOM.render(
-
-    <Provider store={store}> 
-    <App/>
-    </Provider>
-    , document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
