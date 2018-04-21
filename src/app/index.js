5e1755a2b2dabc4d21200f53d8a0313640ab6b2b
radmilatomic <rada1007@gmail.com>
2018-04-21 21:06:00 +0200

    added redirection to login route when starting the app; redirection to home route still not enabled

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Main from '../components/main/index.js';
 import Home from '../containers/home/index.js';
+
+
 import LogIn from '../containers/logIn/logIn.js';
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
     
    
-    	render() {
-    		return(
-    			<div>
-					{/*<LogIn/>*/}
-                <Main/>
-
-    			</div>
-
+        render() {
+    return (
+      <div>
+        <Router>
+        <div>
+           
+            <Main/>
+           
+          </div>
+        </Router>
+      </div>
+      
     );
-}
+  }
 }
 
 export default App;
--- src/containers/logIn/logIn.js
+++ src/containers/logIn/logIn.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-/*import Main from '../../containers/logIn/index.js';*/
+import {
+  Redirect,
+} from "react-router-dom";
+
 import './css.css';
+import auth from '../../components/auth'
 
 class LogIn extends Component {
  constructor(props) {
@@ -9,80 +13,75 @@ class LogIn extends Component {
    email: '',
    password: '',
    mesage: '',
-   redirectToReferrer: false
+   redirectToReferrer:false,
   }
  }
-
- login() {
-  fakeAuth.authenticate(() => {
-   this.setState({ redirectToReferrer: true });
-  });
- };
-
 
  signUp() {
-  /* Validating email adress*/
-  const email = this.state.email;
-  // console.log(this.state.email);
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(re.test(email)) {
-   console.log("sve true");
-  }
-  else {
-   this.setState((prevProps) => {
-    return console.log("aaaa" + prevProps);
-   });
-   console.log(this.state.mesage);
-  }
+// /* Validating email adress*/
+//   const email = this.state.email;
+//   // console.log(this.state.email);
+//   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   if(re.test(email)) {
+//    console.log("sve true");
+//   }
+//   else {
+//    this.setState((prevProps) => {
+//     return console.log("aaaa"+ prevProps);
+//    });
+//    console.log(this.state.mesage);
+
+//   }
+
+auth.authenticate(()=>{
+            this.setState({redirectToReferrer:true})
+        });
+
  }
 
-
-
-
-
  render() {
-  const { from } = this.props.location.state || { from: { pathname: "/" } };
+
   const { redirectToReferrer } = this.state;
 
-
-  if (redirectToReferrer) {
-   return <Redirect to={from} />;
-  }
-
+            if (redirectToReferrer){
+                return <Redirect to={{
+                    pathname:'/',
+                }}
+                />
+            }
   return (
-    <form>
+  <form>
      <h2>Sign In</h2>
-     <button onClick={this.login}>Log in</button>
+
      <div className="group">
       <input
-        type="text"
-        required="required"
-        onChange={(event) => this.setState({email: event.target.value})}
+             type="text"
+             required="required"
+             onChange={(event) => this.setState({email: event.target.value})}
       />
       <span className="highlight"></span>
       <span className="bar"></span>
       <label>Email</label>
      </div>
 
-     <div className="group">
-      <input
-        type="password"
-        required="required"
-        onChange={(event) => this.setState({password: event.target.value})}
-      />
-      <span className="highlight"></span>
-      <span className="bar"></span>
-      <label>password</label>
-     </div>
+   <div className="group">
+    <input
+      type="password"
+      required="required"
+      onChange={(event) => this.setState({password: event.target.value})}
+    />
+    <span className="highlight"></span>
+    <span className="bar"></span>
+    <label>password</label>
+   </div>
 
 
-     <div className="btn-box">
-      <button className="btn btn-submit"
-              type="submit"
-              onClick={() => this.signUp()}>LogIn
-      </button>
-     </div>
-    </form>
+      <div className="btn-box">
+       <button className="btn btn-submit"
+               type="submit"
+               onClick={() => this.signUp()}>LogIn</button>
+      </div>
+  </form>
 
   );
  }
